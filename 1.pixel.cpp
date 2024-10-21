57ec422589d4b1af4cde4e5afd031e2b6ce4ae67
Kyle van Niekerk <kylevn185@gmail.com>
2024-10-21 20:59:27 +0200

    Decrease Render Scale

--- 1.pixel.cpp
+++ 1.pixel.cpp
@@ -1,17 +1,19 @@
 #include <SDL2/SDL.h>
 
 int main(){
+	// Initialise Window and Renderer
 	SDL_Window* window = nullptr;
 	SDL_Renderer* renderer = nullptr;
-
+	
 	SDL_Init(SDL_INIT_VIDEO);
-	SDL_CreateWindowAndRenderer(640*4, 480*4, 0, &window, &renderer);
+	SDL_CreateWindowAndRenderer(640*1.5, 480*1.5, 0, &window, &renderer);
 	SDL_RenderSetScale(renderer,4,4);
 
+	// Draw black screen
 	SDL_SetRenderDrawColor(renderer, 0,0,0,255);
 	SDL_RenderClear(renderer);
 
-
+	// Render Single White Pixel
 	SDL_SetRenderDrawColor(renderer,255,255,255,255);
 	SDL_RenderDrawPoint(renderer, 640/2, 480/2);
 
