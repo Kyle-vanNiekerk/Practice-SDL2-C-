8b874025cab44beab26e6fc195b7b2c34d5dd730
Kyle van Niekerk <kylevn185@gmail.com>
2024-10-21 21:27:00 +0200

    Convert from C++ to C

---
+++ 1.pixel.c
@@ -0,0 +1,26 @@
+#include <SDL2/SDL.h>
+#include <stddef.h> // For nullptr
+
+int main(){
+	// Initialise Window and Renderer
+	SDL_Window* window = NULL; // C++ nullptr
+	SDL_Renderer* renderer = NULL;
+	
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_CreateWindowAndRenderer(640*1.5, 480*1.5, 0, &window, &renderer);
+	SDL_RenderSetScale(renderer,4,4);
+
+	// Draw black screen
+	SDL_SetRenderDrawColor(renderer, 0,0,0,255);
+	SDL_RenderClear(renderer);
+
+	// Render Single White Pixel
+	SDL_SetRenderDrawColor(renderer,255,255,255,255);
+	SDL_RenderDrawPoint(renderer, 640/2, 480/2);
+
+	SDL_RenderPresent(renderer);
+
+	SDL_Delay(10000);
+
+	return 0;
+}
